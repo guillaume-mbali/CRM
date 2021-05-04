334d01414ec2763be8b1876cf43a6fd2bb6b79df
guillaumemb <guillaume.mbali72@gmail.com>
2021-05-04 18:42:02 +0200

    Ajout d'une proposition d'identifiants de connexion

--- assets/js/pages/LoginPage.jsx
+++ assets/js/pages/LoginPage.jsx
@@ -70,7 +70,7 @@ const LoginPage = ({history}) => {
                 </button>
             </div>
         </form>
-        <p>Vous pouvez utiliser comme adresse email : guillaume.mbali72@gmail.com et comme mot de passe : password</p>
+        <p>Pour vous connecté, vous pouvez utiliser comme adresse email : pierre.dupond@gmail.com et comme mot de passe : password</p>
         </>
     );
 }
